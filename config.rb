e389485ef4de2bc0dd01b3d372aab34b9d77f365
Yoshiya Hinosawa <stibium121@gmail.com>
2014-11-26 21:40:46 +0000

    Create constants in config.rb

--- config.rb
+++ config.rb
@@ -3,11 +3,17 @@
 
 require 'yaml'
 
-BASE_DIR = ENV['BASE_DIR']
+BASE_DIR = ENV['BASE_DIR'] || root
 
-set :source, 'm'
+DEFAULT_MIDDLEMAN_SOURCE = 'm'
+DEFAULT_JAVASCRIPT_SOURCE = 'src'
+DEFAULT_BUILD_DIR = 'p'
+DEFAULT_INFRASTRUCTURE_SOURCE = 'infrastructure'
+DEFAULT_LANGUAGE_CODE = 'en'
 
-set :build_dir, File.join(BASE_DIR, ENV['BUILD_DIR'] || 'build')
+set :source, DEFAULT_MIDDLEMAN_SOURCE
+
+set :build_dir, File.join(BASE_DIR, (ENV['BUILD_DIR'] || DEFAULT_BUILD_DIR))
 
 set :css_dir, 'stylesheets'
 
@@ -23,10 +29,10 @@ ready do
   sprockets.append_path File.join BASE_DIR, 'bower_components'
 
   # main implementations
-  sprockets.append_path File.join root, 'src'
+  sprockets.append_path File.join root, DEFAULT_JAVASCRIPT_SOURCE
 
   # infrastructure implementations in the parent project
-  sprockets.append_path File.join BASE_DIR, 'infrastructure'
+  sprockets.append_path File.join BASE_DIR, DEFAULT_INFRASTRUCTURE_SOURCE
 end
 
 
@@ -39,7 +45,7 @@ config = {}
 # develop mode configuration
 configure :development do
 
-  config['language'] = 'en'
+  config['language'] = DEFAULT_LANGUAGE_CODE
 
 end
 
