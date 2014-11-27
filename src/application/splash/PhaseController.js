5f604b03756153b11b81dcb5f64366a1833a7cf6
Yoshiya Hinosawa <stibium121@gmail.com>
2014-11-27 03:09:48 +0000

    Clean up around splash screen

---
+++ src/application/splash/PhaseController.js
@@ -0,0 +1,57 @@
+pages = window.pages || {};
+pages.splash = pages.splash || {};
+
+/**
+ * @class
+ * PhaseController controls the phases of the splash page.
+ */
+pages.splash.PhaseController = (function () {
+    'use strict';
+
+    var ANIM_DUR = 1400;
+
+    var exports = function () {
+    };
+
+    var spcPrototype = exports.prototype;
+
+    spcPrototype.start = function () {
+        var that = this;
+
+        var dur = ANIM_DUR;
+
+        return this.logoAnim('images/kt3k-studio.svg', dur).then(function () {
+
+            return that.logoAnim('images/straw-logo.svg', dur);
+
+        }).then(function () {
+
+            that.goToTitle();
+
+        });
+
+    };
+
+    spcPrototype.goToTitle = function () {
+        location.replace('title.html');
+    };
+
+    spcPrototype.logoAnim = function (path, dur) {
+        var that = this;
+
+        return loadImage(path, 'splash-logo', document.body).then(function ($img) {
+
+            $img.click(function () {
+                that.goToTitle();
+            });
+
+            return $img.anim('logo-anim', dur).then(function () {
+                $img.remove();
+            });
+
+        });
+    };
+
+    return exports;
+
+}());
