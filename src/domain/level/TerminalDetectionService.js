45dd0f68ef99949a7ec0e3799903c9059c4e57e9
Yoshiya Hinosawa <stibium121@gmail.com>
2014-12-06 15:38:13 +0900

    Add TerminalDetectionService (placeholder)

---
+++ src/domain/level/TerminalDetectionService.js
@@ -0,0 +1,18 @@
+
+/**
+ * @class
+ * TerminalDetectionService provide the functionality of detecting the termination condition of the game.
+ */
+domain.level.TerminalDetectionService = (function () {
+    'use strict';
+
+    var exports = function () {
+    };
+
+    var tdsPt = exports.prototype;
+
+    tdsPt.x = 1;
+
+    return exports;
+
+}());
