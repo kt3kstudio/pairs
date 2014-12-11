1aee9580a3e7d185b6a9c753fa99c1e439aced91
Yoshiya Hinosawa <stibium121@gmail.com>
2014-12-12 00:18:43 +0900

    Add UserActivity model

---
+++ src/datadomain/UserActivity.js
@@ -0,0 +1,29 @@
+
+/**
+ * @class
+ * UserActivity class records miscellaneous activity of user.
+ *
+ * This class stores meta information about the user activity, such as
+ * having done tutorial, reviewing app, first launch or not etc.
+ */
+datadomain.UserActivity = (function () {
+    'use strict';
+
+    var exports = function () {
+        this.doneFirstSwipe = opts.doneFirstSwipe;
+        this.doneFirstFusion = opts.doneFirstFusion;
+        this.doneStartGame = opts.doneStartGame;
+        this.doneReviewRemind = opts.doneReviewRemind;
+    };
+
+    exports.createFromObject = function (object) {
+        return new exports(object);
+    };
+
+    var uaPt = exports.prototype;
+
+    uaPt.a = 1;
+
+    return exports;
+
+}());
