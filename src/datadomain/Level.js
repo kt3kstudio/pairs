18577d1ad276d06eb5a3e1c3f55432889a6bda8b
Yoshiya Hinosawa <stibium121@gmail.com>
2014-12-12 00:20:32 +0900

    Change level model

--- src/datadomain/Level.js
+++ src/datadomain/Level.js
@@ -6,12 +6,34 @@
 datadomain.Level = (function () {
     'use strict';
 
-    var exports = function () {
+    var exports = function (id, name, goals, threshold0, threshold1, cells) {
+        this.id = id;
+        this.name = name;
+        this.goals = goals;
+        this.threshold0 = threshold0;
+        this.threshold1 = threshold1;
+        this.cells = cells;
     };
 
     var levelPt = exports.prototype;
 
-    levelPt.a = 1;
+    levelPt.stars = function (score) {
+
+        if (score < this.threshold0) {
+
+            return 1;
+
+        } else if (score < this.threshold1) {
+
+            return 2;
+
+        } else {
+
+            return 3;
+
+        }
+
+    };
 
     return exports;
 
