ba85374cd28dc7daf21d28d35dfe815c5450e58e
Yoshiya Hinosawa <stibium121@gmail.com>
2015-01-02 18:08:05 +0900

    Add doc of the datadomain.goal.Goal

--- src/datadomain/goal/Goal.js
+++ src/datadomain/goal/Goal.js
@@ -1,7 +1,19 @@
 
+
+/**
+ * @class
+ * @abstract
+ *
+ * Goal class is the model class of the goal of the level.
+ */
 datadomain.goal.Goal = (function () {
     'use strict';
 
+    /**
+     * @constructor
+     * @param {String} type The type of the goal
+     * @param {Object} [options] The options
+     */
     var exports = function (type, options) {
         this.type = type;
         this.options = options;
@@ -9,6 +21,11 @@ datadomain.goal.Goal = (function () {
 
     var gPt = exports.prototype;
 
+    /**
+     * Gets string representation.
+     *
+     * @abstract
+     */
     gPt.toString = function () {
     };
 
