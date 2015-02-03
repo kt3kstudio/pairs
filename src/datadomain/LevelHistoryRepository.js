31f88e24c5d27e8da7f8ff7f626c3f4ffbf0f48b
Yoshiya Hinosawa <stibium121@gmail.com>
2015-02-04 00:58:13 +0900

    Add LevelHistoryRepository

---
+++ src/datadomain/LevelHistoryRepository.js
@@ -0,0 +1,18 @@
+
+
+
+/**
+ * LevelHistoryRepository is the repository class for LevelHistory model.
+ */
+datadomain.LevelHistoryRepository = subclass(function (pt) {
+    'use strict';
+
+    /**
+     * Gets the level histories by the floor.
+     *
+     * @param {String} floorId The floor id
+     * @return {Promise}
+     */
+    pt.getCollectionByFloorId = function (floorId) {
+    };
+});
