e0fb2810e87ef2fdd555c9a6a2d82e0d64251aa6
Yoshiya Hinosawa <stibium121@gmail.com>
2015-02-14 19:11:08 +0900

    Add Cell model

---
+++ src/datadomain/Cell.js
@@ -0,0 +1,17 @@
+
+
+
+/**
+ * The cell.
+ *
+ * @class
+ * @valueObject
+ */
+datadomain.Cell = subclass(function (pt) {
+    'use strict';
+
+    pt.constructor = function (gene) {
+        this.gene = gene;
+    };
+
+});
