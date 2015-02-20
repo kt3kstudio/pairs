7d6c5a9c00c03937cee0221ad556a4e7a3899459
Yoshiya Hinosawa <stibium121@gmail.com>
2015-02-20 23:59:49 +0900

    Add doc to Level model

--- src/datadomain/Level.js
+++ src/datadomain/Level.js
@@ -9,9 +9,26 @@
 datadomain.Level = subclass(function (pt) {
     'use strict';
 
-    pt.constructor = function (name, goal, cells) {
-        this.name = name;
+    /**
+     * @constructor
+     * @param {String} id The id
+     * @param {datadomain.goal.Goal} goal The goal
+     * @param {datadomain.CellCollection} cells The collection of the cells
+     */
+    pt.constructor = function (id, goal, cells) {
+        /**
+         * @property {String} id The id
+         */
+        this.id = id;
+
+        /**
+         * @property {datadomain.goal.Goal} goal The goal
+         */
         this.goal = goal;
+
+        /**
+         * @property {datadomain.CellCollection} cells The collection of the cells
+         */
         this.cells = cells;
     };
 
