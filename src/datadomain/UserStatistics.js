d184f126a3ac48b83f87c19993c67b00e56b7573
Yoshiya Hinosawa <stibium121@gmail.com>
2015-02-21 17:28:29 +0900

    Add UserStatistics model

---
+++ src/datadomain/UserStatistics.js
@@ -0,0 +1,26 @@
+
+
+
+
+/**
+ * UserStatistics is the collection class of user statistics info.
+ *
+ */
+datadomain.UserStatistics = subclass(function (pt) {
+    'use strict';
+
+    /**
+     * @constructor
+     * @param {Object} opts The options
+     * @param {Number} [opts.launchTimes] The number of the launches of the app
+     */
+    pt.constuctor = function (opts) {
+
+        /**
+         * @property {Number} launchTimes The number of the launches of the app
+         */
+        this.launchTimes = opts.launchTimes;
+
+    };
+
+});
