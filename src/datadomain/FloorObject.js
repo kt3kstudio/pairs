d9b58caf793435cda611ffa772cc1f09fc46459a
Yoshiya Hinosawa <stibium121@gmail.com>
2015-02-26 01:17:07 +0900

    Add FloorObject constructor

--- src/datadomain/FloorObject.js
+++ src/datadomain/FloorObject.js
@@ -12,8 +12,24 @@
 datadomain.FloorObject = subclass(function (pt) {
     'use strict';
 
-    pt.constructor = function () {
+    /**
+     * @constructor
+     * @param {String} id The id
+     * @param {String} type The type
+     * @param {Object} offset The offset
+     * @param {Number} offset.x The x coordinate
+     * @param {Number} offest.y The y coordinate
+     * @param {Object} size The size
+     * @param {Number} size.w The width
+     * @param {Number} size.h The height
+     * @param {Object} opts The options
+     */
+    pt.constructor = function (id, type, offset, size, opts) {
+        this.id = id;
+        this.type = type;
+        this.offset = offset;
+        this.size = size;
+        this.opts = opts;
     };
 
 });
-
