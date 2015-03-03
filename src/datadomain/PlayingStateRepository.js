986c758ac248e368f2806665ab6c6a7ef23f65c5
Yoshiya Hinosawa <stibium121@gmail.com>
2015-03-03 20:04:58 +0900

    Refactor PlayingStateRepository

--- src/datadomain/PlayingStateRepository.js
+++ src/datadomain/PlayingStateRepository.js
@@ -3,34 +3,38 @@
 /**
  * PlayingStateRepository is the repository class for PlayingState model.
  */
-datadomain.PlayingStateRepository = (function () {
+datadomain.PlayingStateRepository = subclass(function (pt) {
     'use strict';
 
     var PLAYING_DATA_KEY = '-playing-state';
 
-    var exports = function () {
-    };
 
-    var pdrPt = exports.prototype;
-
-    pdrPt.getByName = function (name) {
+    /**
+     * Gets a playing state by the id
+     */
+    pt.getById = function (id) {
 
         return infrastructure.storage.get(name + PLAYING_DATA_KEY, null).then(function (data) {
 
-            return data && new datadomain.PlayingState.createFromObject(name, data);
+            return data && new datadomain.PlayingState(data);
 
         });
 
     };
 
-    pdrPt.save = function (playState) {
-        infrastructure.storage.set(playState.name + PLAYING_DATA_KEY, playState.toObject());
+    pt.save = function (playState) {
+
+        return infrastructure.storage.set(playState.name + PLAYING_DATA_KEY, playState.toObject());
+
     };
 
-    pdrPt.clearByName = function (name) {
-        infrastructure.storage.set(name + PLAYING_DATA_KEY, null);
+    /**
+     * Clears the data by the id
+     */
+    pt.clearById = function (id) {
+
+        return infrastructure.storage.set(name + PLAYING_DATA_KEY, null);
+
     };
 
-    return exports;
-
-}());
+});
