240e7a56aaa6ae4b0080f1a8d34a01cba624aceb
Yoshiya Hinosawa <stibium121@gmail.com>
2015-05-17 18:18:19 +0900

    Add NormalSprite

---
+++ src/domain/common/NormalSprite.js
@@ -0,0 +1,121 @@
+
+
+
+/**
+ * Actor with visual representation which has the phases, such as show/hide, appear/disappear.
+ *
+ * @class
+ * @extends domain.common.Actor
+ */
+domain.common.NormalSprite = $.defineActor(function (pt, parent) {
+    'use strict';
+
+    var noop = function () {};
+    var resolved = Promise.resolve();
+
+    pt.showAnim = null;
+    pt.showAnimDur = 500; // default 500ms
+
+    pt.willShow = noop;
+    pt.didShow = noop;
+
+    pt.show = function () {
+
+        var that = this;
+
+        return resolved.then(function () {
+
+            return that.willShow();
+
+        }).then(function () {
+
+            if (that.showAnim && that.showAnimDur) {
+
+                return that.elem.anim(that.showAnim, that.showAnimDur);
+
+            }
+
+        }).then(function () {
+
+            return that.didShow();
+
+        });
+
+    };
+
+    pt.hideAnim = null;
+    pt.hideAnimDur = 500; // default 500ms
+
+    pt.willHide = noop;
+    pt.didHide = noop;
+
+    pt.hide = function () {
+
+        var that = this;
+
+        return resolved.then(function () {
+
+            return that.willHide();
+
+        }).then(function () {
+
+            if (that.hideAnim && that.hideAnimDur) {
+
+                return that.elem.anim(that.hideAnim, that.hideAnimDur);
+
+            }
+
+        }).then(function () {
+
+            return that.didHide();
+
+        });
+
+    };
+
+    pt.willAppear = noop;
+    pt.didAppear = noop;
+
+    pt.appear = function () {
+
+        var that = this;
+
+        return resolved.then(function () {
+
+            return that.willAppear();
+
+        }).then(function () {
+
+            return that.show();
+
+        }).then(function () {
+
+            return that.didAppear();
+
+        });
+
+    };
+
+    pt.willDisappear = noop;
+    pt.didDisappear = noop;
+
+    pt.disappear = function () {
+
+        var that = this;
+
+        return resolved.then(function () {
+
+            return that.willDisappear();
+
+        }).then(function () {
+
+            return that.hide();
+
+        }).then(function () {
+
+            return that.didDisappear();
+
+        });
+    };
+
+});
