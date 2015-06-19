ca73b36764c959dda30568d8f465212159a4a5a0
Yoshiya Hinosawa <stibium121@gmail.com>
2015-06-19 19:50:08 +0900

    Add state sprite test

--- spec/domain/common/StateSprite.js
+++ spec/domain/common/StateSprite.js
@@ -7,9 +7,41 @@ describe('StateSprite', function () {
     describe('setState', function () {
 
         it('sets the image for the appropriate state', function () {
+
+            var sprite = new domain.common.StateSprite($('<img />'));
+
+            sprite.stateImage = {run: '/test.png'};
+
+            sprite.setState('run');
+
+            expect(sprite.elem.attr('src')).to.equal('/test.png');
+
         });
 
         it('throws when no image of the given state', function () {
+
+            var sprite = new domain.common.StateSprite($('<img />'));
+
+            sprite.stateImage = {run: '/test.png'};
+
+            expect(function () {
+
+                sprite.setState('walk');
+
+            }).to.throw(Error);
+
+        });
+
+        it('throws then no stateImage property set', function () {
+
+            var sprite = new domain.common.StateSprite($('<img />'));
+
+            expect(function () {
+
+                sprite.setState('run');
+
+            }).to.throw(Error);
+
         });
 
     });
