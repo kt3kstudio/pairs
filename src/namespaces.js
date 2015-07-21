695a246416b5906de89315d1158844d0af97e7f4
Yoshiya Hinosawa <stibium121@gmail.com>
2015-07-22 02:41:25 +0900

    Remove scene.splash namespace

--- src/namespaces.js
+++ src/namespaces.js
@@ -2,9 +2,7 @@
 // application layer which controlls each scene
 scene = {};
 scene.common = {};
-scene.splash = {};
 scene.title = {};
-scene.map = {};
 scene.level = {};
 
 // application layer which contoll each page
