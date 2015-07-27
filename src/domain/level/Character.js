6ef81c7b8abefe116fa70fc18b3afc8675161bc5
Yoshiya Hinosawa <stibium121@gmail.com>
2015-07-27 12:45:28 +0900

    Hide character after hide method

--- src/domain/level/Character.js
+++ src/domain/level/Character.js
@@ -4,9 +4,19 @@
 domain.level.Character = subclass(domain.common.CharSprite, function (pt, parent) {
     'use strict';
 
-    pt.constructor = function (elem) {
 
-        parent.constructor.call(this, elem);
+    pt.willShow = function () {
+
+        parent.willShow.call(this);
+
+        this.elem.css('display', 'inline');
+    };
+
+    pt.didHide = function () {
+
+        parent.didHide.call(this);
+
+        this.elem.css('display', 'none');
 
     };
 
