24085e13c90b0d3073bfee10c7f719bb47a0dc74
Yoshiya Hinosawa <stibium121@gmail.com>
2015-07-29 12:30:33 +0900

    Clean up and add test of DimensionFactory

---
+++ spec/domain/level/DimensionFactory.js
@@ -0,0 +1,112 @@
+
+
+
+
+describe('DimensionFactory', function () {
+    'use strict';
+
+    var factory;
+
+    before(function () {
+
+        factory = new domain.level.DimensionFactory();
+
+    });
+
+    describe('topUIPosition', function () {
+
+        it('returns the dimension for the top UIs', function () {
+
+            var dimension = factory.topUIPosition();
+
+            expect(dimension).to.be.instanceof(domain.level.Dimension);
+
+        });
+
+    });
+
+    describe('fieldPosition', function () {
+
+        it('returns the dimension for the field', function () {
+
+            var dimension = factory.fieldPosition();
+
+            expect(dimension).to.be.instanceof(domain.level.Dimension);
+
+        });
+
+    });
+
+    describe('evalRoomPosition', function () {
+
+        it('returns the dimension for the evaluation place', function () {
+
+            var dimension = factory.evalRoomPosition();
+
+            expect(dimension).to.be.instanceof(domain.level.Dimension);
+
+        });
+
+    });
+
+    describe('queuePosition', function () {
+
+        it('returns the dimension for the queue', function () {
+
+            var dimension = factory.queuePosition();
+
+            expect(dimension).to.be.instanceof(domain.level.Dimension);
+
+        });
+
+    });
+
+    describe('fusionBoxPosition', function () {
+
+        it('returns the dimension for the fusion place', function () {
+
+            var dimension = factory.fusionBoxPosition();
+
+            expect(dimension).to.be.instanceof(domain.level.Dimension);
+
+        });
+
+    });
+
+    describe('paperPosition', function () {
+
+        it('returns the dimension for the paper', function () {
+
+            var dimension = factory.paperPosition();
+
+            expect(dimension).to.be.instanceof(domain.level.Dimension);
+
+        });
+
+    });
+
+    describe('resultPanePosition', function () {
+
+        it('returns the dimension for the result pane', function () {
+
+            var dimension = factory.resultPanePosition();
+
+            expect(dimension).to.be.instanceof(domain.level.Dimension);
+
+        });
+
+    });
+
+    describe('scoreboardDimension', function () {
+
+        it('returns the dimension for the scoreboard', function () {
+
+            var dimension = factory.scoreboardDimension();
+
+            expect(dimension).to.be.instanceof(domain.level.Dimension);
+
+        });
+
+    });
+
+});
--- src/domain/level/DimensionFactory.js
+++ src/domain/level/DimensionFactory.js
@@ -3,7 +3,7 @@
  *
  * @class
  */
-domain.level.DimensionFactory = (function () {
+domain.level.DimensionFactory = subclass(function (pt) {
     'use strict';
 
     var TOP_UI_HEIGHT = 50; // the height of the score board at the top
@@ -12,21 +12,38 @@ domain.level.DimensionFactory = (function () {
     // height / width
     var PLAY_FIELD_RATIO = 6 / 4;
 
-    var exports = function () {
+    /**
+     * @constructor
+     */
+    pt.constructor = function () {
+
         this.calc();
+
     };
 
-    var posPt = exports.prototype;
+    /**
+     * Calculates the available area in the current window.
+     *
+     * @private
+     */
+    pt.calcAvailableArea = function () {
 
-    posPt.calcAvailableArea = function () {
         var w = this.width = $(window).width();
         var h = this.height = $(window).height();
 
         this.availableHeight = h - TOP_UI_HEIGHT - BOTTOM_UI_HEIGHT;
         this.availableWidth = w;
+
     };
 
-    posPt.calcBestArea = function () {
+
+    /**
+     * Calculates the best fitting playable area for the level scene.
+     *
+     * @private
+     */
+    pt.calcBestArea = function () {
+
         this.calcAvailableArea();
 
         if (this.availableWidth * PLAY_FIELD_RATIO > this.availableHeight) {
@@ -34,76 +51,148 @@ domain.level.DimensionFactory = (function () {
             // height dominant screen
             this.bestWidth = this.availableHeight / PLAY_FIELD_RATIO;
             this.bestHeight = this.availableHeight;
+
         } else {
 
             // width dominant screen
             this.bestWidth = this.availableWidth;
             this.bestHeight = this.availableWidth * PLAY_FIELD_RATIO;
         }
+
     };
 
-    posPt.calcLeft = function () {
+
+    /**
+     * Calculates the leftmost position.
+     *
+     * @private
+     */
+    pt.calcLeft = function () {
+
         this.left = (this.width - this.bestWidth) / 2;
+
     };
 
-    posPt.calc = function () {
+
+    /**
+     * Calculates things which are needed for providing the dimensions of the objects in the level scene.
+     *
+     * @private
+     */
+    pt.calc = function () {
+
         this.calcBestArea();
         this.calcLeft();
 
         this.UNIT = this.bestWidth / 4;
         this.LEFT = this.left + this.UNIT / 2;
         this.TOP = TOP_UI_HEIGHT;
+
     };
 
-    posPt.topUIPosition = function () {
-        return {top: 0, left: this.left};
+
+    /**
+     * Returns the dimension for the top ui component.
+     *
+     * @return {domain.level.Dimension}
+     */
+    pt.topUIPosition = function () {
+
+        return new domain.level.Dimension({top: 0, left: this.left});
+
     };
 
-    posPt.gridPosition = function (x, y, w) {
+
+    /**
+     * Returns the dimension for an object in the grid positions.
+     *
+     * @private
+     * @return {domain.level.Dimension}
+     */
+    pt.gridPosition = function (x, y, w) {
+
         var u = this.UNIT;
 
-        return {top: this.TOP + u * y, left: this.LEFT + u * x, unit: u, width: u * w};
+        return new domain.level.Dimension({top: this.TOP + u * y, left: this.LEFT + u * x, unit: u, width: u * w});
+
     };
 
-    posPt.fieldPosition = function () {
+    /**
+     * Returns the dimension for the field.
+     *
+     * @return {domain.level.Dimension}
+     */
+    pt.fieldPosition = function () {
+
         return this.gridPosition(0, 2, 3);
+
     };
 
-    posPt.evalRoomPosition = function () {
+
+    /**
+     * Returns the dimension for the evaluation room.
+     *
+     * @return {domain.level.Dimension}
+     */
+    pt.evalRoomPosition = function () {
+
         return this.gridPosition(0, 1, 2);
+
     };
 
-    posPt.leftDoorPosition = function () {
-        return this.gridPosition(0, 0, 1);
-    };
 
-    posPt.rightDoorPosition = function () {
-        return this.gridPosition(2, 1, 1);
-    };
+    /**
+     * Returns the dimension for the exit queue. (The unit is a bit smaller.)
+     *
+     * @return {domain.level.Dimension}
+     */
+    pt.queuePosition = function () {
 
-    posPt.queuePosition = function () {
         var pos = this.gridPosition(1, 0, 1);
 
         pos.unit /= 2;
         pos.left -= pos.unit / 4;
 
         return pos;
+
     };
 
-    posPt.fusionBoxPosition = function () {
+    /**
+     * Returns the dimension for the fusion box.
+     *
+     * @return {domain.level.Dimension}
+     */
+    pt.fusionBoxPosition = function () {
+
         var pos = this.gridPosition(1, 1, 1);
 
         pos.unit /= 1.5;
         pos.left -= pos.unit / 4;
 
         return pos;
+
     };
 
-    posPt.paperPosition = function () {
-        return {left: this.width / 2, top: this.TOP + this.UNIT * 4};
+
+    /**
+     * Returns the dimension for the paper.
+     *
+     * @return {domain.level.Dimension}
+     */
+    pt.paperPosition = function () {
+
+        return new domain.level.Dimension({left: this.width / 2, top: this.TOP + this.UNIT * 4});
+
     };
 
-    posPt.resultPanePosition = function () {
+
+    /**
+     * Returns the dimension for the result pane.
+     *
+     * @return {domain.level.Dimension}
+     */
+    pt.resultPanePosition = function () {
+
         var pos = this.gridPosition(0, 2, 3);
 
         pos.left = 15;
@@ -114,22 +203,26 @@ domain.level.DimensionFactory = (function () {
         pos.width -= pos.left * 2;
 
         return pos;
+
     };
+
 
     /**
      * Returns the dimension for the scoreboard.
      *
-     * @return {Dimension}
+     * @return {domain.level.Dimension}
      */
-    posPt.scoreboardDimension = function () {
-        return {
+    pt.scoreboardDimension = function () {
+
+        return new domain.level.Dimension({
+
             left: this.left,
             top: 0,
             width: this.bestWidth / 2,
             height: TOP_UI_HEIGHT
-        };
+
+        });
+
     };
 
-    return exports;
-
-}());
+});
