0dd5d69135e1acfb32578e6a0c5a70b13343ecf5
Yoshiya Hinosawa <stibium121@gmail.com>
2015-07-31 21:35:17 +0900

    Refactor FIG

--- src/util/FieldIndexGenerator.js
+++ src/util/FieldIndexGenerator.js
@@ -3,18 +3,16 @@
  *
  * The list is like a snake on the wall, inspired by the charactor arrangment of the mayan scripture.
  */
-util.FieldIndexGenerator = (function () {
+util.FieldIndexGenerator = subclass(function (pt) {
     'use strict';
 
     /**
      * @constructor
      * @param {Number} [max] The max number of colums on each row
      */
-    var exports = function (max) {
+    pt.constructor = function (max) {
         this.max = max || 3;
     };
-
-    var figPt = exports.prototype;
 
     /**
      * Generates indices.
@@ -23,7 +21,8 @@ util.FieldIndexGenerator = (function () {
      * @param {Array} used The used (unavailable) indices
      * @return {Array}
      */
-    figPt.generate = function (need, used) {
+    pt.generate = function (need, used) {
+
         var results = [];
         var ip = new IndexPointer(this.max);
 
@@ -49,65 +48,63 @@ util.FieldIndexGenerator = (function () {
      * @class util.FieldIndexGenerator.IndexPointer
      * @private
      */
-    var IndexPointer =
+    var IndexPointer = subclass(function (pt) {
 
-    /**
-     * @constructor
-     * @param {Number} max The max of number of columns
-     */
-    function (max) {
-        this.x = 0;
-        this.y = 0;
-        this.max = max;
-        this.maxIndex = max - 1;
-    };
+        /**
+         * @constructor
+         * @param {Number} max The max of number of columns
+         */
+        pt.constructor = function (max) {
+            this.x = 0;
+            this.y = 0;
+            this.max = max;
+            this.maxIndex = max - 1;
+        };
 
-    var ipPt = IndexPointer.prototype;
+        /**
+         * Gets the current index as an array.
+         *
+         * @return {Array}
+         */
+        pt.get = function () {
+            return [this.x, this.y];
+        };
 
-    /**
-     * Gets the current index as an array.
-     *
-     * @return {Array}
-     */
-    ipPt.get = function () {
-        return [this.x, this.y];
-    };
+        /**
+         * The pointer goes to the next position.
+         */
+        pt.next = function () {
 
-    /**
-     * The pointer goes to the next position.
-     */
-    ipPt.next = function () {
+            if (this.x % 2 === 0) {
 
-        if (this.x % 2 === 0) {
+                if (this.y >= this.maxIndex) {
 
-            if (this.y >= this.maxIndex) {
+                    this.x += 1;
 
-                this.x += 1;
+                } else {
 
-            } else {
+                    this.y += 1;
 
-                this.y += 1;
-
-            }
-
-        } else {
-
-            if (this.y <= 0) {
-
-                this.x += 1;
+                }
 
             } else {
 
-                this.y -= 1;
+                if (this.y <= 0) {
+
+                    this.x += 1;
+
+                } else {
+
+                    this.y -= 1;
+
+                }
 
             }
 
-        }
+            return this.get();
 
-        return this.get();
+        };
 
-    };
+    });
 
-    return exports;
-
-}());
+});
