37fc34aed9a032559d3ccb30bd0e4c866ea06fe9
Yoshiya Hinosawa <stibium121@gmail.com>
2015-08-05 13:23:45 +0900

    Add test of character-on-level

---
+++ spec/domain/level/Character.js
@@ -0,0 +1,40 @@
+
+
+
+describe('domain.level.Character', function () {
+
+    beforeEach(function () {
+
+        this.$dom = $('<span />');
+
+        this.$dom.data('character', {id: 'ma'});
+
+        this.character = new domain.level.Character(this.$dom);
+
+    });
+
+    describe('willShow', function () {
+
+        it('sets the display of the element inline', function () {
+
+            this.character.willShow();
+
+            expect(this.character.elem.css('display')).to.equal('inline');
+
+        });
+
+    });
+
+    describe('didHide', function () {
+
+        it('sets the display of the element none', function () {
+
+            this.character.didHide();
+
+            expect(this.character.elem.css('display')).to.equal('none');
+
+        });
+
+    });
+
+});
