a099a302c5a4fc7e77f77224a756bbaeeeb26d86
Yoshiya Hinosawa <stibium121@gmail.com>
2015-10-09 22:07:33 +0900

    Make coverage report on default test run

--- karma.js
+++ karma.js
@@ -69,7 +69,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+    reporters: ['spec', 'coverage'],
 
 
     // web server port
