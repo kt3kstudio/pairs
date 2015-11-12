a71c971a9eea8b63d4c30b3a7cecb60705b32b1d
Yoshiya Hinosawa <stibium121@gmail.com>
2015-11-12 14:31:39 +0900

    Update FrogSprite using new dimension

--- src/domain/map/FrogSprite.js
+++ src/domain/map/FrogSprite.js
@@ -8,21 +8,31 @@
  *
  * @extends domain.common.StayRunSprite
  */
-domain.map.FrogSprite = subclass(domain.common.StayRunSprite, function (pt) {
-    'use strict';
+domain.map.FrogSprite = subclass(domain.common.StayRunSprite, function (pt, parent) {
+    'use strict'
 
-    pt.leftStayImage = 'images/frog-stay.out.svg';
-    pt.leftRunImage = 'images/frog-run.out.svg';
-
-    pt.w = 100;
-    pt.h = 50;
-
-    pt.x = 30;
-    pt.y = 100;
+    pt.leftStayImage = 'images/frog-stay.out.svg'
+    pt.leftRunImage = 'images/frog-run.out.svg'
 
     pt.awayDur = 400;
     pt.awayAnimDur = 400;
 
-});
+    pt.constructor = function () {
 
-$.cc.assign('frog', domain.map.FrogSprite);
+        parent.constructor.apply(this, arguments)
+
+        this.x = 40
+        this.y = 100
+
+        this.dimension = new domain.common.Dimension({
+            width: 100,
+            height: 50,
+            ratioX: 0.5,
+            ratioY: 1
+        })
+
+    }
+
+})
+
+$.cc.assign('frog', domain.map.FrogSprite)
