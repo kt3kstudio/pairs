924954827eb55afda922850ea54421e92de74236
Yoshiya Hinosawa <stibium121@gmail.com>
2015-11-20 18:09:41 +0900

    Add Grid model

---
+++ spec/domain/common/Grid.js
@@ -0,0 +1,39 @@
+describe('domain.common.Grid', function () {
+  'use strict'
+
+  var grid
+
+  beforeEach(function () {
+    grid = new domain.common.Grid({
+      left: 100,
+      top: 200,
+      unitWidth: 100,
+      unitHeight: 100,
+      cellWidth: 50,
+      cellHeight: 50
+    })
+  })
+
+  describe('getX', function () {
+
+    it('gets the x position of the given grid x position', function () {
+      expect(grid.getX(-2)).to.equal(-100)
+      expect(grid.getX(-1)).to.equal(0)
+      expect(grid.getX(0)).to.equal(100)
+      expect(grid.getX(1)).to.equal(200)
+      expect(grid.getX(2)).to.equal(300)
+      expect(grid.getX(3)).to.equal(400)
+    })
+  })
+
+  describe('getY', function () {
+    it('gets the y position of the given grid x position', function () {
+      expect(grid.getY(-2)).to.equal(0)
+      expect(grid.getY(-1)).to.equal(100)
+      expect(grid.getY(0)).to.equal(200)
+      expect(grid.getY(1)).to.equal(300)
+      expect(grid.getY(2)).to.equal(400)
+      expect(grid.getY(3)).to.equal(500)
+    })
+  })
+})
---
+++ src/domain/common/Grid.js
@@ -0,0 +1,53 @@
+/**
+ * Grid model represents the grid layout.
+ *
+ * The unit of a grid means the rectangle from x_0 to x_1 and from y_0 to x_1
+ * The cell of a grid means the rectangle which is put on each grid point.
+ *
+ * Usually cell width and height are equal to or less then unit width and height resp.,
+ * but this isn't necessarilly so. If the cell width is greater than the unit width, then
+ * each cell could overwrap with the next ones.
+ *
+ * @class
+ */
+domain.common.Grid = subclass(function (pt) {
+  'use strict'
+
+  /**
+   * @param {Object} options The options
+   * @param {Number} options.x The x coordinate
+   * @param {Number} options.y The y coordinate
+   * @param {Number} options.unitWidth The width of the unit
+   * @param {Number} options.unitHeight The height of the unit
+   * @param {Number} options.cellWidth The width of the cell
+   * @param {Number} options.cellHeight The height of the cell
+   */
+  pt.constructor = function (options) {
+    this.x = options.left
+    this.y = options.top
+    this.unitWidth = options.unitWidth
+    this.unitHeight = options.unitHeight
+    this.cellWidth = options.cellWidth
+    this.cellHeight = options.cellHeight
+  }
+
+  /**
+   * Gets the x of the given grid x position.
+   *
+   * @param {Number} x The x position (Integer)
+   * @return {Number}
+   */
+  pt.getX = function (x) {
+    return this.x + this.unitWidth * x
+  }
+
+  /**
+   * Gets the y of the given grid y position.
+   *
+   * @param {Number} y The y position (Integer)
+   * @return {Number}
+   */
+  pt.getY = function (y) {
+    return this.y + this.unitHeight * y
+  }
+})
