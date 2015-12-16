d46c2433cf5d88ac927ed4bdac712daa85ed935b
Yoshiya Hinosawa <stibium121@gmail.com>
2015-12-16 14:12:45 +0900

    Fix test of domain.level.Character

--- spec/domain/level/Character.js
+++ spec/domain/level/Character.js
@@ -2,26 +2,39 @@ describe('domain.level.Character', function () {
     'use strict'
 
     beforeEach(function () {
+
         this.$dom = $('<span />')
 
         this.$dom.data('character', {id: 'ma'})
 
         this.character = new domain.level.Character(this.$dom)
+
+        this.character.setGrid(new domain.common.Grid())
+
     })
 
     describe('willShow', function () {
+
         it('sets the display of the element inline', function () {
+
             this.character.willShow()
 
             expect(this.character.elem.css('display')).to.equal('inline')
+
         })
+
     })
 
     describe('didHide', function () {
+
         it('sets the display of the element none', function () {
+
             this.character.didHide()
 
             expect(this.character.elem.css('display')).to.equal('none')
+
         })
+
     })
+
 })
--- src/domain/level/Character.js
+++ src/domain/level/Character.js
@@ -2,16 +2,21 @@ domain.level.Character = subclass(domain.common.CharSprite, function (pt, parent
     'use strict'
 
     pt.willShow = function () {
-        parent.willShow.call(this)
+
+        parent.willShow.apply(this, arguments)
 
         this.elem.css('display', 'inline')
+
     }
 
     pt.didHide = function () {
+
         parent.didHide.call(this)
 
         this.elem.css('display', 'none')
+
     }
+
 })
 
 $.cc.assign('character-on-level', domain.level.Character)
