6a81e9d053cbcda6757992e2fb3f83cb18d30bba
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-08 23:54:11 +0900

    Add babelify transform

--- bulbofile.babel.js
+++ bulbofile.babel.js
@@ -1,16 +1,27 @@
 import {asset} from 'bulbo'
 import loadPlugins from 'gulp-load-plugins'
+import through from 'through'
+import browserify from 'browserify'
 const $ = loadPlugins()
 const config = {lang: 'en'}
 
-asset('m/**/*.js', {base: 'm/'})
+asset('m/**/*.js', {
+    read: false,
+    watch: ['m/**/*.js', 'src/**/*.js']
+})(src =>
+    src
+    .pipe(through(function (file) {
+        file.contents = browserify(file.path).transform('babelify').bundle()
+        this.queue(file)
+    })))
 
-asset('m/*.html', {base: 'm/'})(src => src
-  .pipe($.frontMatter())
-  .pipe($.wrap({src: 'm/layouts/layout.nunjucks'}, {configJSON: JSON.stringify(config)}, {engine: 'nunjucks'})))
+asset('m/*.html')(src =>
+    src
+    .pipe($.frontMatter())
+    .pipe($.wrap({src: 'm/layouts/layout.nunjucks'}, {configJSON: JSON.stringify(config)}, {engine: 'nunjucks'})))
 
 asset([
-  'm/data/**/*.*',
-  'm/images/**/*.*',
-  'm/stylesheets/**/*.*'
+    'm/data/**/*.*',
+    'm/images/**/*.*',
+    'm/stylesheets/**/*.*'
 ], {base: 'm/'})
