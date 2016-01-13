acfdc85c679f299c828deb49d2efea3750ecb1e8
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-14 00:36:32 +0900

    Add padding

--- src/domain/common/Image.js
+++ src/domain/common/Image.js
@@ -11,12 +11,14 @@ domain.common.Image = subclass(function (pt) {
      * @param {Boolean} mirrorY If the image is mirrored by y-axis
      */
     pt.constructor = function (src, mirrorX, mirrorY) {
+
         this.src = src
         this.mirrorX = mirrorX
         this.mirrorY = mirrorY
 
         this.scaleX = this.mirrorX ? -1 : 1
         this.scaleY = this.mirrorY ? -1 : 1
+
     }
 
     /**
@@ -25,9 +27,11 @@ domain.common.Image = subclass(function (pt) {
      * @param {jQuery} elem The element to apply the image info (needs to be <img> jquery object)
      */
     pt.apply = function (elem) {
+
         elem.css('transform', this.makeTransform())
 
         elem.attr('src', this.src)
+
     }
 
     /**
@@ -37,6 +41,9 @@ domain.common.Image = subclass(function (pt) {
      * @return {String}
      */
     pt.makeTransform = function () {
+
         return 'scale(' + this.scaleX + ', ' + this.scaleY + ')'
+
     }
+
 })
