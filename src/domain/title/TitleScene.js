12cdb222fa5ae0f1bc2a4d01e49823a2227e2820
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-19 22:09:25 +0900

    Refactor TitleScene

--- src/domain/title/TitleScene.js
+++ src/domain/title/TitleScene.js
@@ -1,69 +1,77 @@
 import Actor from '../common/Actor'
 
+const event = $.cc.event
+
 /**
  * TitleScene class handles the motions sequences of the title scene.
  */
-domain.title.TitleScene = subclass(Actor, function (pt) {
-    'use strict'
+class TitleScene extends Actor {
 
-    pt.getMenuBtn = function () {
+    getMenuBtn() {
         return $('.menu-button-root').cc.get('menu-button')
     }
 
     /**
      * Entry point of the title scene.
      */
-    pt.start = function () {
-        var that = this
+    @event('scene-start')
+    start() {
 
-        loadImage('images/title-logo.svg', 'title-logo elem', this.elem).then(function ($img) {
-            return $img.anim('title-appear', 2000).then(function () {
-                return $img.animation('float 6000ms infinite')
-            })
-        })
+        loadImage('images/title-logo.svg', 'title-logo elem', this.elem).then(($img) => $img.anim('title-appear', 2000).then(() => $img.animation('float 6000ms infinite')))
 
-        wait(500).then(function () {
+        wait(500).then(() => {
+
             that.getMenuBtn().show()
 
             $('<p />')
                 .text('GET UP')
                 .addClass('touch-here elem')
                 .appendTo(that.elem)
-                .click(function () {
-                    that.goToMap()
-                })
+                .click(() => this.goToMap())
                 .anim('title-appear', 1000)
-                .then(function ($p) {
-                    $p.animation('float 1000ms infinite')
-                })
-        }).catch(function (e) { console.log(e) })
-    }.event('scene-start')
+                .then((p) => p.animation('float 1000ms infinite'))
 
-    pt.reset = function () {
-        this.fadeOut().then(function () {
-            location.reload()
         })
-    }.event('scene-reset')
 
-    pt.fadeOut = function () {
+    }
+
+    @event('scene-reset')
+    reset() {
+
+        this.fadeOut().then(() => location.reload())
+
+    }
+
+    fadeOut() {
+
         var p = this.getMenuBtn().hide()
 
-        var q = $('.elem').css('opacity', 0).anim('disappear', 500).then(function () {
+        var q = $('.elem').css('opacity', 0).anim('disappear', 500).then(() => {
+
             $('.elem').remove()
 
             return wait(100)
+
         })
 
         return Promise.all([p, q])
+
     }
 
-    pt.goToMap = function () {
-        this.fadeOut().then(function () {
-            return ui.common.BackgroundService.turnBlack()
-        }).then(function () {
+    goToMap() {
+
+        this.fadeOut()
+
+        .then(() => ui.common.BackgroundService.turnBlack())
+
+        .then(() => {
+
             location.href = 'map.html'
-        })
-    }
-})
 
-$.cc.assign('title-scene', domain.title.TitleScene)
+        })
+
+    }
+
+}
+
+$.cc.assign('title-scene', TitleScene)
