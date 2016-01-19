6ab13233ebfa449251c07173f370e0713b911979
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-19 23:16:29 +0900

    Refactor title-scene

--- src/domain/title/TitleScene.js
+++ src/domain/title/TitleScene.js
@@ -5,10 +5,17 @@ const event = $.cc.event
 /**
  * TitleScene class handles the motions sequences of the title scene.
  */
-class TitleScene extends Actor {
+class TitleScene extends domain.common.SceneContext {
 
+    /**
+     * Gets the menu button.
+     *
+     * @return {MenuButton}
+     */
     getMenuBtn() {
+
         return $('.menu-button-root').cc.get('menu-button')
+
     }
 
     /**
@@ -17,16 +24,18 @@ class TitleScene extends Actor {
     @event('scene-start')
     start() {
 
-        loadImage('images/title-logo.svg', 'title-logo elem', this.elem).then(($img) => $img.anim('title-appear', 2000).then(() => $img.animation('float 6000ms infinite')))
+        loadImage('images/title-logo.svg', 'title-logo elem', this.elem)
+
+        .then(($img) => $img.anim('title-appear', 2000).then(() => $img.animation('float 6000ms infinite')))
 
         wait(500).then(() => {
 
-            that.getMenuBtn().show()
+            this.getMenuBtn().show()
 
             $('<p />')
                 .text('GET UP')
                 .addClass('touch-here elem')
-                .appendTo(that.elem)
+                .appendTo(this.elem)
                 .click(() => this.goToMap())
                 .anim('title-appear', 1000)
                 .then((p) => p.animation('float 1000ms infinite'))
@@ -35,29 +44,24 @@ class TitleScene extends Actor {
 
     }
 
-    @event('scene-reset')
-    reset() {
-
-        this.fadeOut().then(() => location.reload())
-
-    }
-
+    /**
+     * Fades out the scene.
+     */
     fadeOut() {
 
-        var p = this.getMenuBtn().hide()
-
-        var q = $('.elem').css('opacity', 0).anim('disappear', 500).then(() => {
+        return Promise.all([this.getMenuBtn().hide(), $('.elem').css('opacity', 0).anim('disappear', 500).then(() => {
 
             $('.elem').remove()
 
             return wait(100)
 
-        })
-
-        return Promise.all([p, q])
+        })])
 
     }
 
+    /**
+     * Transions to the map scene.
+     */
     goToMap() {
 
         this.fadeOut()
