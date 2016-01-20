051bf9305a2184bdb926926ec226ca7fcc5eea7e
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-20 14:14:05 +0900

    Remove unused namespaces

--- src/namespaces.js
+++ src/namespaces.js
@@ -1,14 +1,9 @@
 window.subclass = $.cc.subclass
 
-// application layer which controlls each scene
-window.scene = {}
-window.scene.level = {}
-
 // game logic domain
 window.domain = {}
 window.domain.common = {}
 window.domain.splash = {}
-window.domain.title = {}
 window.domain.map = {}
 window.domain.level = {}
 window.domain.genetics = {}
