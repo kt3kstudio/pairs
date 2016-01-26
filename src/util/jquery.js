109844d133e75f27104b3fd90348f30a2ba3e890
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-26 13:55:38 +0900

    Remove unnecessary IIFE

--- src/util/jquery.js
+++ src/util/jquery.js
@@ -1,85 +1,77 @@
 import reflow from 'spn/lib/reflow'
+
 /**
- * jQuery class
+ * Performs the animation.
  *
- * @class jQuery
+ * @param {String} animation The css animation
  */
-;(function ($) {
-    'use strict'
+$.fn.animation = function (animation) {
 
-    /**
-     * Performs the animation.
-     *
-     * @param {String} animation The css animation
-     */
-    $.fn.animation = function (animation) {
+    this.css('-webkit-animation', '')
+    reflow(this)
+    this.css('-webkit-animation', animation)
 
-        this.css('-webkit-animation', '')
-        reflow(this)
-        this.css('-webkit-animation', animation)
+    return this
 
-        return this
+}
 
-    }
+/**
+ * Performs the animation in the duration and returns a promise.
+ *
+ * @param {String} animation The animation name
+ * @param {Number} dur The duration of the animation
+ * @return {Promise}
+ */
+$.fn.anim = function (animation, dur) {
 
-    /**
-     * Performs the animation in the duration and returns a promise.
-     *
-     * @param {String} animation The animation name
-     * @param {Number} dur The duration of the animation
-     * @return {Promise}
-     */
-    $.fn.anim = function (animation, dur) {
+    this.animation(animation + ' ' + dur + 'ms')
 
-        this.animation(animation + ' ' + dur + 'ms')
+    return wait(dur, this)
 
-        return wait(dur, this)
+}
 
-    }
+/**
+ * Binds event once and returns promise.
+ *
+ * @param {String} events The list of events names
+ * @return {Promise}
+ */
+$.fn.once = function (events) {
 
-    /**
-     * Binds event once and returns promise.
-     *
-     * @param {String} events The list of events names
-     * @return {Promise}
-     */
-    $.fn.once = function (events) {
+    return new Promise((resolve) => this.one(events, resolve))
 
-        return new Promise((resolve) => this.one(events, resolve))
+}
 
-    }
+/**
+ * Returns Observable of an event stream.
+ *
+ * @param {String} events The list of event names
+ * @return {Rx.Observable}
+ */
+$.fn.streamOf = function (events) {
 
-    /**
-     * Returns Observable of an event stream.
-     *
-     * @param {String} events The list of event names
-     * @return {Rx.Observable}
-     */
-    $.fn.streamOf = function (events) {
+    return Rx.Observable.fromEvent(this, events)
 
-        return Rx.Observable.fromEvent(this, events)
+}
 
-    }
+/**
+ * Returns a promise which resolves when image is loaded. Only works with `img` tag.
+ *
+ * @return {Promise}
+ */
+$.fn.imageLoaded = function () {
 
-    /**
-     * Returns a promise which resolves when image is loaded. Only works with `img` tag.
-     *
-     * @return {Promise}
-     */
-    $.fn.imageLoaded = function () {
+    return new Promise((resolve, reject) =>
 
-        return new Promise((resolve, reject) =>
+        this
 
-            this
+        .on('error', () => reject(new Error('image can not be loaded: ' + this.attr('src'))))
 
-            .on('error', () => reject(new Error('image can not be loaded: ' + this.attr('src'))))
+        .on('load', () => resolve())
 
-            .on('load', () => resolve())
+        .attr('src', this.attr('src'))
 
-            .attr('src', this.attr('src'))
+    )
 
-        )
+}
 
-    }
-
-}(window.jQuery))
