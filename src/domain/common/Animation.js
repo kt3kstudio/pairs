24db51bbcf022d7492c87abe14c801a117486c27
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-26 13:58:43 +0900

    Use spn.Animation

--- src/domain/common/Animation.js
+++ src/domain/common/Animation.js
@@ -1,34 +1,6 @@
-const ANIMATION_PROP_NAME = '-webkit-animation'
-import reflow from 'spn/lib/reflow'
+import {Animation} from 'spn'
 
 /**
  * Animation class represents the css animation.
  */
-export default class Animation {
-
-    /**
-     * @param {String} name The name of the css animation (keyframes)
-     * @param {Number} duration The duration of the animation
-     */
-    constructor(name, duration) {
-
-        this.name = name
-        this.duration = duration
-
-    }
-
-    /**
-     * @param {jQuery} dom The jQuery dom element
-     * @return {Promise}
-     */
-    apply(dom) {
-
-        dom.css(ANIMATION_PROP_NAME, '')
-        reflow(dom)
-        dom.css(ANIMATION_PROP_NAME, this.name + ' ' + this.duration + 'ms')
-
-        return wait(this.duration)
-
-    }
-
-}
+export default Animation
