02334c13e40cdf2f9d5b730b8aa3e46ae1131a35
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-31 22:30:24 +0900

    Remove unnecessary IIFE

--- src/util/rx.js
+++ src/util/rx.js
@@ -1,134 +1,92 @@
+
+const Rx = window.Rx
+
 /**
- * RxJS
+ * Checks if it's flatMappable or not.
  *
- * @class Rx
- * @singleton
+ * @param {Object} x Testing object
+ * @return {Boolean}
  */
-;(function (Rx) {
-    'use strict'
+Rx.helpers.isObservableLike = x => x instanceof Rx.Observable || Rx.helpers.isPromise(x)
 
-    /**
-     * Rx.helpers
-     *
-     * @class Rx.helpers
-     * @singleton
-     */
+const wrapUnobservable = x => Rx.helpers.isObservableLike(x) ? x : [x]
 
-    /**
-     * Checks if it's flatMappable or not.
-     *
-     * @param {Object} x Testing object
-     * @return {Boolean}
-     */
-    Rx.helpers.isObservableLike = function (x) {
-        return x instanceof Rx.Observable || Rx.helpers.isPromise(x)
-    }
+/**
+ * Maps it and flatMap it only when it's possible.
+ *
+ * @param {Function} f Mapping function
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.pipe = function (f) {
 
-    var wrapUnobservable = function (x) {
-        return Rx.helpers.isObservableLike(x) ? x : [x]
-    }
+    return this.map(f).flattenObservable()
 
-    /**
-     * Rx.Observable
-     * @class Rx.Observable
-     */
+}
 
-    /**
-     * Maps it and flatMap it only when it's possible.
-     *
-     * @param {Function} f Mapping function
-     * @return {Rx.Observable}
-     */
-    Rx.Observable.prototype.pipe = function (f) {
-        return this.map(f).flattenObservable()
-    }
+/**
+ * Flattens it.
+ *
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.flattenObservable = function () {
 
-    /**
-     * Flattens it.
-     *
-     * @return {Rx.Observable}
-     */
-    Rx.Observable.prototype.flattenObservable = function () {
-        return this.map(wrapUnobservable).flatMap(function (x) {
-            return x
-        })
-    }
+    return this.map(wrapUnobservable).flatMap(x => x)
 
-    /**
-     * Filters null equivalent element.
-     *
-     * @return {Rx.Observable}
-     */
-    Rx.Observable.prototype.filterNull = function () {
-        return this.filter(function (x) {
-            return x != null
-        })
-    }
+}
 
-    /**
-     * Returns promise which resolves the last value of the stream when the stream completed.
-     *
-     * @return {Promise}
-     */
-    Rx.Observable.prototype.getPromise = function () {
-        var source = this
+/**
+ * Filters null equivalent element.
+ *
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.filterNull = function () {
 
-        return new Promise(function (resolve, reject) {
-            source.takeLast(1).subscribe(function (x) {
-                resolve(x)
-            }, function (error) {
-                reject(error)
-            }, function () {
-                resolve()
-            })
-        })
-    }
+    return this.filter(x => x != null)
 
-    /**
-     * Emit to the given dom element. assume the observable is a stream of the event.
-     *
-     * @param {jQuery|HTMLElement|String} dom The dom to emit event
-     * @return {Object}
-     */
-    Rx.Observable.prototype.emitInto = function (dom) {
-        dom = $(dom)
+}
 
-        var source = this
+/**
+ * Returns promise which resolves the last value of the stream when the stream completed.
+ *
+ * @return {Promise}
+ */
+Rx.Observable.prototype.getPromise = function () {
 
-        return source.forEach(function (event) {
-            dom.trigger(event)
-        })
-    }
+    return new Promise((resolve, reject) => this.takeLast(1).subscribe(x => resolve(x), error => reject(error), () => resolve()))
 
-    /**
-     * Hooks the function to the stream
-     *
-     * @param {Function} f The hooking function
-     * @return {Rx.Observable}
-     */
-    Rx.Observable.prototype.hook = function (f) {
+}
 
-        return this.filter(function (item) {
+/**
+ * Emit to the given dom element. assume the observable is a stream of the event.
+ *
+ * @param {jQuery|HTMLElement|String} dom The dom to emit event
+ * @return {Object}
+ */
+Rx.Observable.prototype.emitInto = function (dom) {
 
-            f(item)
+    return this.forEach(event => $(dom).trigger(event))
 
-            return true
+}
 
-        })
+/**
+ * Hooks the function to the stream
+ *
+ * @param {Function} f The hooking function
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.hook = function (f) {
 
-    }
+    return this.filter(item => (f(item) || true))
 
-    /**
-     * Array.
-     *
-     * @class Array
-     */
-    /**
-     * Makes into flattenned stream.
-     *
-     * @return {Rx.Observable}
-     */
-    window.Array.prototype.toFlatStream = function () {
-        return Rx.Observable.of.apply(null, this).flattenObservable()
-    }
-}(window.Rx))
+}
+
+/**
+ * Makes into flattenned stream.
+ *
+ * @return {Rx.Observable}
+ */
+window.Array.prototype.toFlatStream = function () {
+
+    return Rx.Observable.of.apply(null, this).flattenObservable()
+
+}
