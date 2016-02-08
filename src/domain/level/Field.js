921684fe807ecd7c7a088c1cb39ac2e6ae86fe20
Yoshiya Hinosawa <stibium121@gmail.com>
2016-02-08 13:47:32 +0900

    Refactor Field

--- src/domain/level/Field.js
+++ src/domain/level/Field.js
@@ -1,30 +1,30 @@
 import {Animation} from 'spn'
-import DimensionalBeing from '../common/DimensionalBeing'
+import Body from '../common/body'
+
+const {compnent} = $.cc
 
 /**
  * Field class represents the background field graphics.
  *
  * This class doesn't handle the mechanism above the field, which is the responsibility of FieldCells and BallMoveMobLeaveService classes.
  */
-domain.level.Field = subclass(DimensionalBeing, function (pt, parent) {
-    'use strict'
+@component('field-grid')
+export default class Field extends Body {
 
-    pt.showAnim = () => new Animation('field-appear', 200)
+    showAnim() { return new Animation('field-appear', 200) }
 
-    pt.hideAnim = () => new Animation('field-disappear', 400)
+    hideAnim() { return new Animation('field-disappear', 400) }
 
     /**
      * @param {Rect} rect The rect to fit into
      */
-    pt.setRect = function (rect) {
+    setRect(rect) {
 
-        parent.setRect.apply(this, arguments)
+        super.setRect(rect)
 
         this.posture.marginX = -5
         this.posture.marginY = -5
 
     }
 
-})
-
-$.cc.assign('field-grid', domain.level.Field)
+}
