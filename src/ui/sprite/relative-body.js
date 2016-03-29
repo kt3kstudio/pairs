d6170247e82b6366931d9e19d17a5da35c2058a2
Yoshiya Hinosawa <stibium121@gmail.com>
2016-03-29 10:08:11 +0900

    Add relative-body doc

--- src/ui/sprite/relative-body.js
+++ src/ui/sprite/relative-body.js
@@ -1,7 +1,14 @@
 
-
+/**
+ * RelativeBody is a trait class which works in relative scale with its parent rect.
+ */
 export default class RelativeBody {
 
+    /**
+     * Handler when the parent rect is set.
+     * This method should be called with its parent rect before initial rendering.
+     * @param {Rect} rect The parent rect
+     */
     onSetParentRect(rect) {
 
         this.x = rect.left + rect.width() * this.relX
