27bfcf3c79f2557f29b7bf9ae7e865fe2bf591d9
Yoshiya Hinosawa <stibium121@gmail.com>
2016-04-13 23:41:55 +0900

    Update bulbo

--- bulbofile.babel.js
+++ bulbofile.babel.js
@@ -5,25 +5,26 @@ import browserify from 'browserify'
 
 const $ = loadPlugins()
 const config = {lang: 'en'}
-const SITE = 'site/'
-const SRC = 'src/'
+const SITE = 'site'
+const SRC = 'src'
 
-asset(`${SITE}**/*.js`, {
-    read: false,
-    watch: [`${SITE}**/*.js`, `${SRC}**/*.js`]
-})(src => src.pipe(through(function (file) {
+asset(`${ SITE }/**/*.js`, {read: false})
+.watch(`${ SITE }/**/*.js`)
+.watch(`${ SRC }/**/*.js`)
+.build(src => src.pipe(through(function (file) {
     file.contents = browserify(file.path).transform('babelify').bundle()
     this.queue(file)
 })))
 
-asset(`${SITE}*.html`, {
-    watch: [`${SITE}*.html`, `${SITE}layout/*.nunjucks`]
-})(src => src
+asset(`${ SITE }/*.html`)
+.watch(`${ SITE }/*.html`)
+.watch(`${ SITE }/layout/*.nunjucks`)
+.build(src => src
     .pipe($.frontMatter())
-    .pipe($.wrap({src: `${SITE}layouts/layout.nunjucks`}, {configJSON: JSON.stringify(config)}, {engine: 'nunjucks'})))
+    .pipe($.wrap({src: `${ SITE }/layouts/layout.nunjucks`}, {configJSON: JSON.stringify(config)}, {engine: 'nunjucks'})))
 
 asset([
-    `${SITE}data/**/*.*`,
-    `${SITE}img/**/*.*`,
-    `${SITE}css/**/*.*`
-], {base: SITE})
+    `${ SITE }/data/**/*.*`,
+    `${ SITE }/img/**/*.*`,
+    `${ SITE }/css/**/*.*`
+]).base(SITE)
