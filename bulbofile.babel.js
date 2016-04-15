0b7f3ac93284f8161343d7088b1b7aa79881b888
Yoshiya Hinosawa <stibium121@gmail.com>
2016-04-15 21:16:30 +0900

    Update bulbo and bulbofile

--- bulbofile.babel.js
+++ bulbofile.babel.js
@@ -8,23 +8,24 @@ const config = {lang: 'en'}
 const SITE = 'site'
 const SRC = 'src'
 
-asset(`${ SITE }/**/*.js`, {read: false})
+// js
+asset(`${ SITE }/**/*.js`)
+.assetOptions({read: false})
 .watch(`${ SITE }/**/*.js`)
 .watch(`${ SRC }/**/*.js`)
-.build(src => src.pipe(through(function (file) {
+.pipe(through(function (file) {
     file.contents = browserify(file.path).transform('babelify').bundle()
     this.queue(file)
-})))
+}))
 
+// html
 asset(`${ SITE }/*.html`)
 .watch(`${ SITE }/*.html`)
 .watch(`${ SITE }/layout/*.nunjucks`)
-.build(src => src
-    .pipe($.frontMatter())
-    .pipe($.wrap({src: `${ SITE }/layouts/layout.nunjucks`}, {configJSON: JSON.stringify(config)}, {engine: 'nunjucks'})))
+.pipe($.frontMatter())
+.pipe($.wrap({src: `${ SITE }/layouts/layout.nunjucks`}, {configJSON: JSON.stringify(config)}, {engine: 'nunjucks'}))
 
-asset([
-    `${ SITE }/data/**/*.*`,
-    `${ SITE }/img/**/*.*`,
-    `${ SITE }/css/**/*.*`
-]).base(SITE)
+// others
+asset(`${ SITE }/data/**/*.*`).base(SITE)
+asset(`${ SITE }/img/**/*.*`).base(SITE)
+asset(`${ SITE }/css/**/*.*`).base(SITE)
