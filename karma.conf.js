a156a2da2fa9c464faae340856a482125c0abe8d
Yoshiya Hinosawa <stibium121@gmail.com>
2016-04-17 23:26:21 +0900

    Clean up karma settings

--- karma.conf.js
+++ karma.conf.js
@@ -4,17 +4,10 @@ module.exports = function (config) {
     config.set({
         frameworks: ['mocha', 'chai', 'jsmockito-jshamcrest', 'browserify'],
         files: [
-            'site/js/common.js',
-            'site/js/splash.js',
-            'site/js/title.js',
-            'site/js/map.js',
-            'site/js/level.js',
-            'spec/helper/polyfill.js',
-            'spec/helper/reset.js',
+            'spec/helper.js',
             'spec/**/*.js'
         ],
         preprocessors: {
-            'site/**/*.js': ['browserify'],
             'spec/**/*.js': ['browserify']
         },
         browserify: {
---
+++ spec/helper.js
@@ -0,0 +1,7 @@
+import '../site/js/common'
+import '../site/js/splash'
+import '../site/js/title'
+import '../site/js/map'
+import '../site/js/level'
+
+global.localStorage.clear()
