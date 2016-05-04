4e64cf64ad13df1518882cdaf18333cf247bd34c
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-04 18:37:21 +0900

    Add Tether

--- site/js/global.js
+++ site/js/global.js
@@ -2,9 +2,11 @@ import $ from 'jquery'
 import Rx from 'rx-lite'
 import {polyfill} from 'es6-promise'
 import 'es6-symbol/implement'
+import Tether from 'tether'
 
 global.$ = $
 global.jQuery = $
 global.Rx = Rx
+global.Tether = Tether
 
 polyfill()
