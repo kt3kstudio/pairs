707406c3c85bb762a9628b36dea299555a4a9b67
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-08 23:43:17 +0900

    Remove gulp-load-plugins

--- bulbofile.js
+++ bulbofile.js
@@ -3,8 +3,8 @@ const asset = bulbo.asset
 
 const bundler = require('bundle-through')
 const wrapper = require('layout-wrapper')
+const frontMatter = require('gulp-front-matter')
 
-const $ = require('gulp-load-plugins')()
 const config = {lang: 'en'}
 const configJSON = JSON.stringify(config)
 const SITE = 'site'
@@ -12,21 +12,21 @@ const SRC = 'src'
 
 // js
 asset(`${ SITE }/**/*.js`)
-.assetOptions({read: false})
-.watch(`${ SITE }/**/*.js`, `${ SRC }/**/*.js`)
-.pipe(bundler({transform: 'babelify'}))
+  .assetOptions({read: false})
+  .watch(`${ SITE }/**/*.js`, `${ SRC }/**/*.js`)
+  .pipe(bundler({transform: 'babelify'}))
 
 // infrastructure.js
 asset(`${ SRC }/infrastructure/infrastructure.js`)
-.watch(`${ SRC }/infrastructure/*.js`)
-.base(`${ SRC }/infrastructure`)
-.pipe(bundler({transform: 'babelify'}))
+  .watch(`${ SRC }/infrastructure/*.js`)
+  .base(`${ SRC }/infrastructure`)
+  .pipe(bundler({transform: 'babelify'}))
 
 // html
 asset(`${ SITE }/*.html`)
-.watch(`${ SITE }/*.html`, `${ SITE }/layout/*.nunjucks`)
-.pipe($.frontMatter())
-.pipe(wrapper.nunjucks({layout: `${ SITE }/layouts`, data: {configJSON}}))
+  .watch(`${ SITE }/*.html`, `${ SITE }/layout/*.nunjucks`)
+  .pipe(frontMatter())
+  .pipe(wrapper.nunjucks({layout: `${ SITE }/layouts`, data: {configJSON}}))
 
 // others
 asset(`${ SITE }/data/**/*.*`).base(SITE)
