eeaa8e53dc957ac5bad85791c0a54c51d4634dcc
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-11 00:21:53 +0900

    Add tether-drop dep

--- site/js/global.js
+++ site/js/global.js
@@ -3,11 +3,13 @@ import Rx from 'rx-lite'
 import {polyfill} from 'es6-promise'
 import 'es6-symbol/implement'
 import Tether from 'tether'
+import Drop from 'tether-drop'
 
 global.$ = $
 global.jQuery = $
 global.Rx = Rx
 global.Tether = Tether
+global.Drop = Drop
 
 require('class-component')
 
