ab2495d6d486bbb2ca18e15d99a4d5ffeeec59d5
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-20 10:08:11 +0900

    feat(speaker.js): fire events at the begining and the end

--- src/ui/sprite/speaker.js
+++ src/ui/sprite/speaker.js
@@ -16,29 +16,35 @@ export default class Speaker {
      * Speaks the phrase
      *
      * @param {string} speech The contents of the speech
+     * @fires 'speech.started' when the speech started
+     * @fires 'speech.ended' when the speech ended
      */
     speak(speech) {
 
-        const cancelDom = this.elem.data('speech-ok') || this.elem
-        const timeout = +this.elem.data('speech-timeout') || DEFAULT_SPEECH_TIMEOUT
+        this.elem.trigger('speech.started')
 
-        speech.css('text-align', 'left')
-        speech.css('display', 'block')
-        speech.css('line-height', '20px')
-        speech.css('overflow', 'hidden')
+        // const cancelDom = this.elem.data('speech-ok') || this.elem
+        const timeout = +this.elem.data('speech-timeout') || DEFAULT_SPEECH_TIMEOUT
 
         let text = speech.text()
 
         text = ScreenplayManager.renderEmoji(text)
+
+        const speech0 = p(text)
+
+        speech0.css('text-align', 'left')
+        speech0.css('display', 'block')
+        speech0.css('height', '0px')
+        speech0.css('overflow', 'hidden')
 
         const speech1 = p(text)
 
         speech1.css('text-align', 'left')
         speech1.css('display', 'inline')
 
-        const wrapper = div(speech, speech1)
+        const wrapper = div(speech0, speech1)
 
-        const drop = new Drop({
+        const drop = new global.Drop({
             target: this.elem[0],
             content: wrapper.appendTo(document.body)[0],
             classes: 'drop-theme-arrows-bounce',
@@ -46,11 +52,11 @@ export default class Speaker {
             openOn: 'always'
         })
 
-        return wait(0)
-        .then(() => speech1.cc.init('puncher').start())
-        .then(() => wait(500))
+        return speech1.cc.up('puncher').trigger('puncher.start').once('puncher.ended')
+        .then(() => wait(timeout / 10))
         .then(() => drop.close())
         .then(() => wait(500))
+        .then(() => this.elem.trigger('speech.ended'))
 
     }
 
