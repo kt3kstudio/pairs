e3b2dbf64a3a768b76195f5d869d682e772b97bf
Yoshiya Hinosawa <stibium121@gmail.com>
2016-06-16 00:08:53 +0900

    feat(result-pane): show success or failure

--- src/level/component/result-pane.js
+++ src/level/component/result-pane.js
@@ -1,50 +1,50 @@
 import {wait, Body} from 'spn'
-import {div} from 'dom-gen'
+import {div, ul, li} from 'dom-gen'
 
 const {component} = $.cc
 
 /**
  * ResultPane class handles the behaviour of the pane which appears when the game finished with a score.
- *
- * @class
  */
 @component('result-pane')
 export default class ResultPane extends Body {
-
     /**
      * Sets the score.
-     *
      * @param {number} score The score to set
      */
     setScore(score) {
-
         this.score = score
+    }
 
+    /**
+     * @param {boolean} isSuccess True iff the result is successful
+     */
+    setSuccess(isSuccess) {
+        this.isSuccess = isSuccess
     }
 
     /**
      * Sets the number of the stars.
-     *
      * @param {number} star The number of stars
      */
     setStar(star) {
-
         this.star = star
-
     }
 
     /**
      * @override
      */
     willShow() {
-
-        this.elem.append(div({
-            addClass: 'result-content',
-            css: {opacity: 0, position: 'relative'}
-        }, 'score = ' + this.score))
+        this.elem.append(
+            div({addClass: 'result-content', css: {opacity: 0, position: 'relative'}},
+                ul(
+                    li('score: ' + this.score),
+                    li('success: ' + this.isSuccess)
+                )
+            )
+        )
 
         return super.willShow()
-
     }
 
     /**
@@ -54,25 +54,19 @@ export default class ResultPane extends Body {
      * @return {Promise} The promise which resolves when the pane hides
      */
     show(timeout) {
-
         return super.show().then(() => this.showInfoPane(timeout))
-
     }
 
     /**
      * Shows the info pane with the given timeout.
-     *
      * @param {Number} timeout
      * @return {Promise}
      */
     showInfoPane(timeout) {
-
         return this.elem.attr({m: 9, n: 7}).cc.init('multiflip').show()
 
         .then(() => Promise.race([wait(timeout), this.elem.once('click touchstart')]))
 
         .then(() => this.elem.cc.get('multiflip').hide())
-
     }
-
 }
