7fecb87727d15d37a0931e147ddf05e99ef25dd7
Yoshiya Hinosawa <stibium121@gmail.com>
2016-07-18 19:11:18 +0900

    chore(char-sprite): remove deprecated methods

--- src/ui/sprite/char-sprite.js
+++ src/ui/sprite/char-sprite.js
@@ -51,43 +51,6 @@ class CharSprite {
   }
 
   /**
-   * Gets the direction to the target point.
-   * @deprecated
-   * @param {string} coordinate 'x' or 'y'
-   * @param {number} to The position
-   */
-  getDirection (coordinate, to) {
-    if (coordinate === 'x') {
-      return to > this.x ? DIRS.RIGHT : DIRS.LEFT
-    }
-
-    return to > this.y ? DIRS.DOWN : DIRS.UP
-  }
-
-  /**
-   * Moves the sprite to the given position within the given duration.
-   * @deprecated
-   * @param {string} coordinate 'x' or 'y'
-   * @param {number} to The position to go
-   * @param {number} dur The duration of movement in ms
-   */
-  moveTo (coordinate, to, dur) {
-    const dir = this.getDirection(coordinate, to)
-
-    this.turn(dir)
-
-    this.setTransitionDuration(dur)
-
-    if (dir === DIRS.UP || dir === DIRS.DOWN) {
-      this.moveToY(to)
-    } else {
-      this.moveToX(to)
-    }
-
-    return wait(dur)
-  }
-
-  /**
    * Sets at the point.
    * @param {Point} point The point to go to
    */
