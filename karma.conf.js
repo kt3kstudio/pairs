46c92d6d5b5bdeb0457fe907bf84a79a56c5437b
Yoshiya Hinosawa <stibium121@gmail.com>
2016-09-06 11:05:49 +0900

    chore(karma.conf.js): clean up karma settings

--- karma.conf.js
+++ karma.conf.js
@@ -1,37 +1,43 @@
 'use strict'
 
-module.exports = function (config) {
-  config.set({
-    frameworks: ['mocha', 'chai', 'jsmockito-jshamcrest', 'browserify'],
-    files: [
-      'site/**/*.css',
-      'test/helper.js',
-      'test/**/*.js'
-    ],
-    preprocessors: {
-      'test/**/*.js': ['browserify']
-    },
-    browserify: {
-      debug: true,
-      noParse: [
-        'jquery',
-        'rx-lite',
-        'es6-promise',
-        'es6-symbol/implement',
-        'tether',
-        'tether-drop',
-        'multiflip',
-        'multiflip-bubble',
-        '@kt3k/puncher'
-      ],
-      transform: ['babelify']
-    },
-    reporters: ['spec'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Chrome'],
-    singleRun: true
-  })
+const IS_COV = process.env.COV === 'true'
+
+const babelConfig = require('./package').babel
+const reporters = ['spec']
+
+if (IS_COV) {
+  babelConfig.plugins = ['istanbul']
+  reporters.push('coverage')
 }
+
+module.exports = config => config.set({
+  frameworks: ['mocha', 'chai', 'jsmockito-jshamcrest', 'browserify'],
+  files: [
+    'site/**/*.css',
+    'test/helper.js',
+    'test/**/*.js'
+  ],
+  preprocessors: {
+    'test/**/*.js': ['browserify']
+  },
+  browserify: {
+    debug: true,
+    noParse: [
+      'jquery',
+      'rx-lite',
+      'es6-promise',
+      'es6-symbol/implement',
+      'tether',
+      'tether-drop',
+      'multiflip',
+      'multiflip-bubble',
+      '@kt3k/puncher'
+    ],
+    transform: [['babelify', babelConfig]]
+  },
+  reporters,
+  coverageReporters: {type: 'lcov'},
+  autoWatch: false,
+  browsers: ['Chrome'],
+  singleRun: true
+})
