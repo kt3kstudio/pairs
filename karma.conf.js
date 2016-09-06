95316d22394d761f7db923889787256b092fa03b
Yoshiya Hinosawa <stibium121@gmail.com>
2016-09-06 11:46:17 +0900

    chore(karma.conf.js): remove karma.watch.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,22 @@
 'use strict'
 
 const IS_COV = process.env.COV === 'true'
+const IS_WATCH = process.env.WATCH == 'true'
 
 const babelConfig = require('./package').babel
 const reporters = ['spec']
 
+let autoWatch = false
+let singleRun = true
+
 if (IS_COV) {
   babelConfig.plugins = ['istanbul']
   reporters.push('coverage')
+}
+
+if (IS_WATCH) {
+  autoWatch = true
+  singleRun = false
 }
 
 module.exports = config => config.set({
@@ -35,9 +44,7 @@ module.exports = config => config.set({
     ],
     transform: [['babelify', babelConfig]]
   },
-  reporters,
+  reporters, autoWatch, singleRun,
   coverageReporters: {type: 'lcov'},
-  autoWatch: false,
-  browsers: ['Chrome'],
-  singleRun: true
+  browsers: ['Chrome']
 })
