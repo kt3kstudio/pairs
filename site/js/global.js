8ce9f11b9d3e18205b60bc3ea54c8001ab20d317
Yoshiya Hinosawa <stibium121@gmail.com>
2016-09-12 18:26:18 +0900

    :fire: refactor(global.js): remove event decorator

--- site/js/global.js
+++ site/js/global.js
@@ -7,15 +7,6 @@ global.Drop = require('tether-drop')
 
 require('class-component')
 
-$.cc.event = (event, selector, key) => Cls => {
-  if (key == null) {
-    key = selector
-    selector = null
-  }
-
-  $.cc.on(event).at(selector)(Cls.prototype, key)
-}
-
 require('es6-promise').polyfill()
 require('es6-object-assign').polyfill()
 require('es6-symbol/implement')
