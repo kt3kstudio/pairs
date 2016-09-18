838217f735441b429fc50cd1607fca770f004d74
Yoshiya Hinosawa <stibium121@gmail.com>
2016-09-18 17:05:20 +0900

    feat(debug): unlock all button

---
+++ src/debug/asset-unlock-all.js
@@ -0,0 +1,25 @@
+const {component, on} = $.cc
+
+@component
+class AssetUnlockAll {
+  /**
+   * @return {FloorAssetCollection}
+   */
+  getFloorAssets () {
+    return $('.floor-asset-collection').cc.get('floor-asset-collection')
+  }
+
+  /**
+   * Unlocks all the assets in the floor.
+   * @param {object} e The event
+   */
+  @on('click') unlockAll () {
+    const floorAssets = this.getFloorAssets()
+
+    floorAssets.items.forEach(item => {
+      floorAssets.unlockById(item.id)
+    })
+  }
+}
+
+module.exports = AssetUnlockAll
