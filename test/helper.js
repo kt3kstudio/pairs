abcfa65d3cf72771c5dfc8ddb1d18b07347edf0d
Yoshiya Hinosawa <stibium121@gmail.com>
2016-10-04 18:47:38 +0900

    test(helper): fix test helper

--- test/helper.js
+++ test/helper.js
@@ -1,8 +1,8 @@
+require('../src/infrastructure/infrastructure')
 require('../site/js/common')
 require('../site/js/splash')
 require('../site/js/title')
 require('../site/js/floor')
 require('../site/js/level')
-require('../src/infrastructure/infrastructure')
 
 global.localStorage.clear()
