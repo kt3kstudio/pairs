73da0ab9d764bf7eeec0465ec73b5721e84ae99d
Yoshiya Hinosawa <stibium121@gmail.com>
2017-01-04 00:35:04 +0900

    :hammer: refactor(being): refactor being decorator using new Being of spn

--- src/ui/being.js
+++ src/ui/being.js
@@ -1,44 +1,6 @@
 const { traits } = require('traits-decorator')
-const { wait } = require('spn')
+const { Being } = require('spn')
 const { component } = $.cc
-
-class Being {
-  show (dur) {
-    this.$el.addClass('showing')
-
-    return Promise.resolve(typeof this.willShow === 'function' && this.willShow())
-
-      .then(() => {
-        const anim = typeof this.showAnim === 'function' && this.showAnim()
-
-        return anim && anim.apply(this.elem, dur)
-      })
-
-      .then(() => typeof this.constructor.SHOW_DURATION === 'number' && wait(this.constructor.SHOW_DURATION))
-
-      .then(() => typeof this.didShow === 'function' && this.didShow())
-
-      .then(() => this.$el.addClass('shown'))
-  }
-
-  hide (dur) {
-    this.$el.removeClass('shown')
-
-    return Promise.resolve(typeof this.willHide === 'function' && this.willHide())
-
-      .then(() => {
-        const anim = typeof this.hideAnim === 'function' && this.hideAnim()
-
-        return anim && anim.apply(this.elem, dur)
-      })
-
-      .then(() => typeof this.constructor.SHOW_DURATION === 'number' && wait(this.constructor.SHOW_DURATION))
-
-      .then(() => typeof this.didHide === 'function' && this.didHide())
-
-      .then(() => this.$el.removeClass('showing'))
-  }
-}
 
 module.exports = Cls => traits(Being)(component(Cls))
 module.exports.dur = dur => Cls => {
