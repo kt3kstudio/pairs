d1e59928ad2fb16b5940cc02f6de0a89751a368c
Yoshiya Hinosawa <stibium121@gmail.com>
2017-01-07 14:07:48 +0900

    :art: chore: move common lib to common.js

--- site/js/level.js
+++ site/js/level.js
@@ -1,7 +1,3 @@
-require('arrowkeys')
-require('swipe-event/swipe-event')
-require('swipe-event/swipe-cross')
-
 require('../../src/level/intro-scene')
 require('../../src/level/play-scene')
 require('../../src/level/outro-scene')
