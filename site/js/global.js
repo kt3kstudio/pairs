8ea36695077c4952e1834d4874ced8b762dfc836
Yoshiya Hinosawa <stibium121@gmail.com>
2017-01-17 15:36:41 +0900

    :recycle: chore: switch to classcaps

--- site/js/global.js
+++ site/js/global.js
@@ -5,7 +5,9 @@ global.Rx = require('rx-lite')
 global.Tether = require('tether')
 global.Drop = require('tether-drop')
 
-require('class-component')
+var cc = require('classcaps')
+var ccj = require('classcaps/jquery')
+ccj(cc, global.$)
 
 require('es6-promise').polyfill()
 require('es6-object-assign').polyfill()
