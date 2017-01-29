d102b1f0752835081a0608282e82471a87c51653
Yoshiya Hinosawa <stibium121@gmail.com>
2017-01-29 13:13:52 +0900

    test: fix tests

--- src/__tests__/helper.js
+++ src/__tests__/helper.js
@@ -1,8 +1,8 @@
 require('../infrastructure/infrastructure')
-require('../../site/js/common')
-require('../../site/js/splash')
-require('../../site/js/title')
-require('../../site/js/floor')
-require('../../site/js/level')
+require('../common')
+require('../splash')
+require('../title')
+require('../floor')
+require('../level')
 
 global.localStorage.clear()
