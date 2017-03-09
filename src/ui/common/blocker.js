271abbf8d61ab9e61f59a47391bb7093260c7458
Yoshiya Hinosawa <stibium121@gmail.com>
2017-03-09 14:06:00 +0900

    :sparkles: feat(loading): show loading indicator when ui is blocking

--- src/ui/common/blocker.js
+++ src/ui/common/blocker.js
@@ -4,6 +4,13 @@ const BLOCKING_CLASS = 'blocking'
 
 @component('ui-blocker')
 class Blocker {
+
+  __init__ () {
+    const el = document.createElement('img')
+    el.src = `${BASEPATH}/img/loading.svg`
+    this.el.appendChild(el)
+  }
+
   unblock () {
     this.el.classList.remove('blocking')
   }
@@ -11,6 +18,7 @@ class Blocker {
   block () {
     this.el.classList.add('blocking')
   }
+
 }
 
 module.exports = Blocker
